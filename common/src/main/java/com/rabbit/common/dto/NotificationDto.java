3e7c19d9eb124293a760f93ebd5d02b71ff2a9cb
artemKinash <126754117+ArtemKinash47@users.noreply.github.com>
2026-03-30 20:38:02 +0200

    notification repository + dto

---
+++ common/src/main/java/com/rabbit/common/dto/NotificationDto.java
@@ -0,0 +1,29 @@
+package com.rabbit.common.dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationDto implements JsonSerializable {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private Long id;
+    private String message;
+    private LocalDateTime created_at;
+
+    @Override
+    public String toJson() {
+       try {
+           return OBJECT_MAPPER.writeValueAsString(this);
+       } catch (Exception e) {
+           throw new RuntimeException("Failed to get JSON serialization", e);
+       }
+    }
+}
