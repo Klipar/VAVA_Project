bad6950b5e484dd4f450c7491c2691cdba37e3ad
Klipar <bhdnkarpenko123@gmail.com>
2026-04-15 00:56:41 +0200

    fix double send request in failed to auth response

--- server/src/main/java/com/rabbit/server/handler/UserHandler.java
+++ server/src/main/java/com/rabbit/server/handler/UserHandler.java
@@ -310,8 +310,10 @@ public class UserHandler {
                     requestBody.get("password").toString()
                 );
 
-                if (dto == null)
+                if (dto == null){
                     send(exchange, 401, "{\"error\":\"Invalid credentials\"}");
+                    return;
+                }
 
                 send(exchange, 201, mapper.writeValueAsString(dto));
             } catch (IllegalArgumentException e) {
