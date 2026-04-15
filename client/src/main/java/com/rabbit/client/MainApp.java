f16ed54628dda7f03586fbc78b19cac810e4b1d9
AvriolF <roman.khodynchak@gmail.com>
2026-04-15 18:05:56 +0200

    set login page as startup and add token persistence in config

--- client/src/main/java/com/rabbit/client/MainApp.java
+++ client/src/main/java/com/rabbit/client/MainApp.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/rabbit/client/fxml/first_input_page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/rabbit/client/fxml/login_page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
 
         scene.getStylesheets().add(getClass().getResource("/com/rabbit/client/css/style.css").toExternalForm());
