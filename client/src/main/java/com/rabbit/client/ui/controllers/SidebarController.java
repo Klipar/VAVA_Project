fef081fd821919bc16609011d8e04a005c979037
Klipar <bhdnkarpenko123@gmail.com>
2026-04-24 14:22:34 +0200

    add icons to left menu

--- client/src/main/java/com/rabbit/client/ui/controllers/SidebarController.java
+++ client/src/main/java/com/rabbit/client/ui/controllers/SidebarController.java
@@ -23,21 +23,19 @@ public class SidebarController {
     public void initMenu() {
         menuItemsContainer.getChildren().clear();
         UserDto user = Config.getInstance().getUser();
-
         ResourceBundle rb = Config.getInstance().getBundle();
 
-
-        createButton(rb.getString("home_page"), "home-view.fxml");
-        createButton(rb.getString("my_projects_"), "projects-view.fxml");
+        createButton(rb.getString("home_page"), "home-view.fxml", "home.png");
+        createButton(rb.getString("my_projects_"), "projects-view.fxml", "folder.png");
 
         if (user.getRole() != UserRole.MANAGER)
-            createButton(rb.getString("my_tasks"), "my-tasks-view.fxml");
+            createButton(rb.getString("my_tasks"), "my-tasks-view.fxml", "task.png");
 
         if (user.getRole() == UserRole.MANAGER || user.getRole() == UserRole.TEAM_LEADER)
-            createButton(rb.getString("admin_panel"), "admin-view.fxml");
+            createButton(rb.getString("admin_panel"), "admin-view.fxml", "settings.png");
 
-        createButton(rb.getString("notifications"), "notifications-view.fxml");
-        createButton(rb.getString("profile"), "profile-view.fxml");
+        createButton(rb.getString("notifications"), "notifications-view.fxml", "notificatios.png");
+        createButton(rb.getString("profile"), "profile-view.fxml", "person.png");
 
         change_language.setOnAction(event -> {
             if (mainController != null) {
@@ -47,10 +45,29 @@ public class SidebarController {
         });
     }
 
-    private void createButton(String text, String fxmlName) {
+    private void createButton(String text, String fxmlName, String iconName) {
         Button btn = new Button(text);
+
+        try {
+            String imagePath = "/com/rabbit/client/images/icons/" + iconName;
+            javafx.scene.image.Image image = new javafx.scene.image.Image(getClass().getResourceAsStream(imagePath));
+            javafx.scene.image.ImageView imageView = new javafx.scene.image.ImageView(image);
+
+
+            imageView.setFitWidth(20);
+            imageView.setFitHeight(20);
+
+            btn.setGraphic(imageView);
+
+            btn.setGraphicTextGap(10);
+        } catch (Exception e) {
+            System.err.println("Failed to load icon: " + iconName);
+        }
+
         btn.getStyleClass().add("sidebar-button");
         btn.setMaxWidth(Double.MAX_VALUE);
+
+        btn.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
 
         btn.setOnAction(event -> {
             if (mainController != null) {
